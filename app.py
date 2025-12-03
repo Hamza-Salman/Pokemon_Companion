067f748df13ba500962ed60941c6c329a207a91a
Hamza Salman <hamzasalman989@gmail.com>
2025-12-02 22:42:33 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    #TODO: Homepage. Have info on project and features
+    return render_template('/index.html')
+
+@app.route("/pokedex")
+def pokedex():
+    #TODO: Have a list of all pokemon broken down by generations
+    return render_template("/pokedex.html")
+
+@app.route("/pokemon")
+def pokemon():
+    #TODO: 
+    return "Hello, World!"
+
+@app.route("/register")
+def register():
+    #TODO: 
+    return "Hello, World!"
+
+@app.route("/login")
+def login():
+    #TODO: 
+    return "Hello, World!"
+
+@app.route("/logout")
+def logout():
+    #TODO: 
+    return "Hello, World!"
+
+@app.route("/team")
+def team():
+    #TODO: 
+    return "Hello, World!"
+
+@app.route("/battle-helper")
+def battle():
+    #TODO: 
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
